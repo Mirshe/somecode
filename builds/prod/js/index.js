37a45eedc57e4eda970a4c37ef7466a9bf418a8c
Mirshe <laptinek@yahoo.es>
2017-09-01 20:12:18 +0300

    1504285938946

--- builds/prod/js/index.js
+++ builds/prod/js/index.js
@@ -1,3 +1,6 @@
-function aa(a=5,b=7){
-  
+"use strict";
+
+function aa() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 5;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 7;
 }
